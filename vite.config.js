7d441de228601f53025826890b2bcf51909220b1
yano-research <haerin.park@yano.co.jp>
2025-04-22 16:02:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5175,
+//   },
+// })
+
 export default defineConfig({
+  base: '/ghg-test/',  // ✅ 리포지토리 이름
   plugins: [react()],
-  server: {
-    port: 5175,
-  },
 })
